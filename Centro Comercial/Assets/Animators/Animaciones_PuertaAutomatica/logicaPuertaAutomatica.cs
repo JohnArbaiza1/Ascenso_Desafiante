ed6f362f408f41add8b67c271121435c4d4e0272
DIGP1 <diegogarci098@gmail.com>
2024-06-16 13:54:49 -0600

    Creacion de puerta automatica

---
+++ Centro Comercial/Assets/Animators/Animaciones_PuertaAutomatica/logicaPuertaAutomatica.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logicaPuertaAutomatica : MonoBehaviour
+{
+    
+    public Animator animator;
+    private void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.Play("AbrirPuertas");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        animator.Play("CerrarPuertas");
+    }
+}
